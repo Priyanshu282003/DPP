9a07cdd29f59282f34bd703f2697e2d44f3d3de6
Priyanshu282003 <dubeypriyanshu0009@gmail.com>
2024-08-30 10:26:30 +0530

    This is day3 gfg

--- day2gfg.cpp
+++ day2gfg.cpp
@@ -1,12 +1,12 @@
 class Solution {
   public:
-    // Function to find the length of a loop in the linked list.
+    
     int countNodesinLoop(Node *head) {
         Node *slow = head;
         Node *fast = head;
         bool loop = false;
 
-        // Detecting the loop using Floyd’s Cycle-Finding Algorithm
+        
         while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
@@ -16,7 +16,7 @@ class Solution {
             }
         }
 
-        // If loop is found, count the number of nodes in the loop
+        
         if (loop) {
             int cnt = 1;
             slow = slow->next;
@@ -27,7 +27,7 @@ class Solution {
             return cnt;
         }
 
-        // If no loop is found, return 0
-        return 0;// Code here
+        
+        return 0;
     }
 };
